e8ecfcb6f903d9930bfb4a639cedda0a825caa27
Raul772 <raulvictor772@gmail.com>
2023-03-25 15:09:37 -0300

    Getting to know ASP.NET

--- AppModelo/src/RL.ModelApp/Program.cs
+++ AppModelo/src/RL.ModelApp/Program.cs
@@ -1,16 +1,19 @@
 
 using Microsoft.AspNetCore.Mvc;
+using RL.ModelApp.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMvc();
-
+builder.Services.AddControllersWithViews(options => options.EnableEndpointRouting = false);
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Definição de Rotas
+app.MapControllerRoute(
+        name: "Default",
+        pattern: "{controller}/{action}/{id?}",
+        defaults: new {controller = "Home", action = "Index"}
+    );
 
-app.UseMvc(routes =>
-{
-    routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
-});
+app.Run();
