a60b6cf943c60e4aa8a2817373cb94b13959bc5f
Raul772 <raulvictor772@gmail.com>
2023-03-25 16:12:34 -0300

    Delete previous test solutions

--- AppModelo/src/RL.ModelApp/Program.cs
+++ AppModelo/src/RL.ModelApp/Program.cs
@@ -1,19 +1,26 @@
 
 using Microsoft.AspNetCore.Mvc;
-using RL.ModelApp.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews(options => options.EnableEndpointRouting = false);
+//  Adição do MVC no projeto
+//  Atribuilçao
+builder.Services.AddControllersWithViews();
+//  Adição do Razor Pages (equivalente ao services.AddMvc();)
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Definição de Rotas
 app.MapControllerRoute(
-        name: "Default",
-        pattern: "{controller}/{action}/{id?}",
-        defaults: new {controller = "Home", action = "Index"}
+        /* name: */ "Default",
+        /* pattern: */ "{controller}/{action}/{id?}",
+        /* defaults: */ new {controller = "Home", action = "Index"}
     );
 
+//  Defaults pode ser utilizado para setar valores padrão a partir de um objeto anonimo
+//  Para mais rotas, chama-se mais vezes o método
+
+
+// Utiliza-se app.Run() ao invés de app.UseMvc()
 app.Run();
